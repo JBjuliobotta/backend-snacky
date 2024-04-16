9db9f3f51d24c8a0f51b696ddbd3795e595df09b
Julio Rafael Botta <jrb12494@gmail.com>
2024-04-16 04:59:54 -0300

    Se agregó el hash del password al crear el usuario y agregaron validaciones correspondientes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,14 +3,26 @@ const UserController=require('../controllers/userController');
 const UserRoutes=(base, app)=>{
 
     const controller=new UserController();
-    app.post(`${base}/create`, async(req, res, next)=>{
+
+    app.post(`${base}/create-admin`, async(req, res, next)=>{
         try {
-            const {email, password, role}=req.body;
-            const response=await controller.CreateNewUser(email, password, role);
-            return res.status(201).json({message: 'Éxito al crear el usuario'});
+            const {email, password}=req.body;
+            await controller.CreateNewAdmin(email, password);
+            return res.status(201).json({message: "Éxito al crear el usuario"});
         } catch (error) {
-            console.error('Error al crear un nuevo usuario-->', error);
-           return res.status(500).json({message: 'Ocurrió un error al intentar crear el usuario'});
+            console.error("Error al crear un nuevo usuario-->", error);
+           return res.status(500).json({message: "Ocurrió un error al intentar crear el usuario"});
+        }
+    });
+
+    app.post(`${base}`, async(req, res, next)=>{
+        try {
+            const {email, password}=req.body;
+            await controller.CreateNewUser(email, password);
+            return res.status(201).json({message: "Éxito al crear el usuario"});
+        } catch (error) {
+            console.error("Error al crear un nuevo usuario-->", error);
+           return res.status(500).json({message: "Ocurrió un error al intentar crear el usuario"});
         }
     });
 
@@ -19,10 +31,10 @@ const UserRoutes=(base, app)=>{
             const id=req.params.id;
             const response=await controller.DeleteUserById(id);
             console.log("USUARIO ELIMINADO-->", JSON.stringify(response));
-            return res.status(200).json({message: 'Éxito al eliminar el usuario'});
+            return res.status(200).json({message: "Éxito al eliminar el usuario"});
         } catch (error) {
-            console.error('Error al eliminar un usuario-->', error);
-            return res.status(500).json({message: 'Ocurrió un error al eliminar el usuario'});
+            console.error("Error al eliminar un usuario-->", error);
+            return res.status(500).json({message: "Ocurrió un error al eliminar el usuario"});
         }
     })
 
