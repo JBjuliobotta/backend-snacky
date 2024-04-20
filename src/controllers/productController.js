6b5cef9838bd0e9b38f481ba76cccdcbaf216366
Julio Rafael Botta <jrb12494@gmail.com>
2024-04-19 23:16:08 -0300

    se completó la config inicial de la ddbb de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -17,7 +17,50 @@ class ProductController{
         } catch (error) {
             throw error;
         }
+    };
+
+    async GetAllProducts(filter){
+        try {
+            let finalResponse=[];
+            if(filter!==undefined){
+                finalResponse=await ProductModel.find();
+            }else {
+                finalResponse=await ProductModel.find({
+                    category: filter
+                });
+            }
+            return finalResponse;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    async GetById(id){
+        try {
+            const product=await ProductModel.findById(id);
+            return product;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    async UpdateProduct(product){
+        try {
+            await ProductModel.findByIdAndUpdate(product._id, product);
+        } catch (error) {
+            throw error
+        }
+    };
+
+    async DeleteProduct(id){
+        try {
+            await ProductModel.findByIdAndDelete(id);
+        } catch (error) {
+            throw error
+        }
     }
+
+
 };
 
 module.exports=ProductController;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -14,6 +14,51 @@ const ProductRoutes=(base, app)=>{
             return res.status(500).json({message: "Ocurrió un error al intentar crear un nuevo producto"});
         }
     });
+
+    app.get(`${base}/`, async(req, res)=>{
+        try {
+            const {filter}=req.query
+            console.log("Valor de la query filter-->", filter);
+            const response=await controller.GetAllProducts(filter);
+            return res.status(200).json(response);
+        } catch (error) {
+            console.error("Error al obtener todos los productos-->", error);
+            return res.status(500).json({message:"Ocurrió un error al intentar obtener el producto"});
+        }
+    });
+
+    app.get(`${base}/:id`, async(req, res)=>{
+       try {
+        const {id}=req.params;
+        const response=await controller.GetById(id);
+        return res.status(200).json(response);
+       } catch (error) {
+            console.log(`Error al obtener el producto con id: ${id}-->`, error);
+            return res.status(500).json({message: "Ocurrió un error al intentar obtener el producto"});
+       } 
+    });
+
+    app.put(`${base}/update`, Auth.isAuth, Auth.isAdmin, async (req, res)=>{
+        try {
+            const product=req.body;
+            await controller.UpdateProduct(product);
+            return res.status(200).json({message: "Éxito al actualizar el producto"});
+        } catch (error) {
+            console.log(`Error al actualizar un producto -->`, error);
+            return res.status(500).json({message: "Ocurrió un error al intentar actualizar un producto"});
+        }
+    });
+
+    app.delete(`${base}/:id`, Auth.isAuth, Auth.isAdmin, async(req, res)=>{
+        try {
+            const {id}=req.params;
+            await controller.DeleteProduct(id);
+            return res.status(200).json({message: "Éxito al eliminar el producto"});
+        } catch (error) {
+            console.log(`Error al eliminar un producto -->`, error);
+            return res.status(500).json({message: "Ocurrió un error al intentar eliminar el producto"});
+        }
+    })
 }
 
 module.exports=ProductRoutes;
